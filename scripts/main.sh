bb6fa2ee69745c99788c2ec15d2d998574611771
Aditya <bluenerd@protonmail.com>
2023-01-09 15:53:14 +0530

    Add 7.3 - prepare virtual kernel filesystems

--- scripts/main.sh
+++ scripts/main.sh
@@ -401,3 +401,22 @@ echo "Chapter 7"
 echo "Changing ownership to root"
 su -c "bash $PWD/chapter7/7.2-changing-ownership.sh"
 
+# Exit on error
+check_exit_code
+if [ $exit_status -ne 0 ]
+then
+	stop_script "chapter7/7.2-changing-ownership.sh"
+fi
+
+# Chapter 7.3
+# ===========
+echo "Preparing virtual kernel filesystems"
+su -c "bash $PWD/chapter7/7.3-prepare-virtual-fs.sh"
+
+# Exit on error 
+check_exit_code
+if [ $exit_status -ne 0 ]
+then
+	stop_script "chapter7/7.3-prepare-virtual-fs.sh"
+fi
+

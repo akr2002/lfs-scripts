7eb86e2c3f1b2752d2236e9fea30bd40d4d85307
Aditya <bluenerd@protonmail.com>
2023-01-10 15:43:29 +0530

    Add 8.16 - Build expect

---
+++ scripts/chapter8/8.16-expect.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+cd /sources
+tar xf expect5.45.4.tar.gz
+cd expect5.45.4
+
+./configure --prefix=/usr           \
+            --with-tcl=/usr/lib     \
+            --enable-shared         \
+            --mandir=/usr/share/man \
+            --with-tclinclude=/usr/include
+
+if [ $? -ne 0 ]
+then
+  exit 1
+fi
+
+make -j5
+
+if [ $? -ne 0 ]
+then
+  exit 1
+fi
+
+make test
+
+make install
+ln -svf expect5.45.4/libexpect5.45.4.so /usr/lib
+
+cd /sources
+rm -rf expect5.45.4
+
--- scripts/main.sh
+++ scripts/main.sh
@@ -726,3 +726,15 @@ then
 	stop_script "chapter8/8.15-tcl.sh"
 fi
 
+# Chapter 8.16
+# ============
+echo "Building expect"
+su -c "bash $SCRIPT/chapter8/8.16-expect.sh"
+
+# Exit on error
+check_exit_code
+if [ $exit_status -ne 0 ]
+then
+	exit 1
+fi
+

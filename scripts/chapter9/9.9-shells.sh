999964450261044de38e5e5b028be07101427e1a
Aditya <bluenerd@protonmail.com>
2023-01-14 21:56:44 +0530

    Add 9.9 - Add /etc/shells

---
+++ scripts/chapter9/9.9-shells.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cat > /etc/shells.sh << "EOF"
+# Begin /etc/shells
+
+/bin/sh
+/bin/bash
+
+# End /etc/shells
+EOF
+
--- scripts/main.sh
+++ scripts/main.sh
@@ -1535,3 +1535,15 @@ then
 	stop_script "chapter9/9.8-inputrc.sh"
 fi
 
+# Chapter 9.9
+# ===========
+echo "Creating /etc/shells"
+su -c "bash $SCRIPT/chapter9/9.9-shells.sh"
+
+# Exit on error
+check_exit_code
+if [ $exit_status -ne 0 ]
+then
+	stop_script "chapter9/9.9-shells.sh"
+fi
+
